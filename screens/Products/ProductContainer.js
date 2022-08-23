3e13432a056bc84ac0f8c668d6136da776510e1a
Oluwatimilehin Oreoluwa Idowu <oluwatimilehin.id@gmail.com>
2022-08-23 20:42:14 +0100

    Add the product screen and its components

---
+++ screens/Products/ProductContainer.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+import {
+  View,
+  StyleSheet,
+  ActivityIndicator,
+  Text,
+  FlatList,
+} from 'react-native';
+import ProductList from './ProductList';
+
+const ProductContainer = () => {
+  return (
+    <View>
+      <Text>Product Container Screen</Text>
+    </View>
+  );
+};
+
+export default ProductContainer;
